2c8eaa0b3bbd109954776032c4cdce4f7082132b
Dhairya Shah <dhairyaj2004@gmail.com>
2024-06-07 09:46:47 +0530

    new

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -12,9 +12,9 @@ function page() {
           type="email"
           id="email"
           className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
-          placeholder="andrew@mail.com"
+          placeholder="dhairya123@gmail.com"
           required
-          value=""
+          
         />
       </div>
       <div className="mb-4">
@@ -25,7 +25,7 @@ function page() {
           className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
           placeholder="*********"
           required
-          value=""
+          
         />
       </div>
       <div className="flex items-center justify-center mb-4">
