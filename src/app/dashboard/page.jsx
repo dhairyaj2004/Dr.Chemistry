3707cac2f91f944c6c65e37f019fa40981a58412
Dhairya Shah <dhairyaj2004@gmail.com>
2024-08-24 17:27:02 +0530

    n

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSession, signOut } from "next-auth/react";
-const ProtectedRoute = dynamic(() => import('../../components/ProtectedRoute'));
+import ProtectedRoute from "../../components/ProtectedRoute";
 import React, { useState, useEffect } from "react";
 
 const Dashboard = () => {
