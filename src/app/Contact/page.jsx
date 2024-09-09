3b10fd1e0b50b2cbb4c17ecc5621eac2d217df7a
Dhairya Shah <dhairyaj2004@gmail.com>
2024-09-09 22:36:58 +0530

    new

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
                 <div className="mt-8 overflow-hidden">
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="p-2 mr-2 bg-gray-100 rounded-lg">
-                            <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
+                            <h1 className="text-3xl sm:text-4xl text-red-500 font-extrabold tracking-tight">
                                 Get in touch: 
                             </h1>
                             <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
@@ -116,22 +116,22 @@ export default function Contact() {
 
                             <div className="flex flex-col mt-2">
                                 <label for="tel" className="hidden">
-                                    Number
+                                    Message
                                 </label>
-                                <input
+                                <textarea
                                     type="tel"
                                     name="tel"
                                     id="tel"
-                                    placeholder="Telephone Number"
+                                    placeholder="Message"
                                     className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
                                 />
                             </div>
 
                             <button
                                 type="submit"
-                                className="md:w-32 bg-purple-500 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-purple-700 transition ease-in-out duration-300"
+                                className="md:w-full bg-purple-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-purple-700 transition ease-in-out duration-300"
                             >
-                                Submit
+                                Send Message
                             </button>
                         </form>
                     </div>
